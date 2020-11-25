509f67916059a34d2bb5a954cde67be31828fd45
Avi Chatraee <avida.chatraee2001@yahoo.com>
2020-11-25 12:18:03 +0330

    Finished show checkout form

--- assets/js/Classes.js
+++ assets/js/Classes.js
@@ -1,51 +1,55 @@
 // Calculate Insurance Class
 class Insurance {
-  constructor() {
+  constructor(car, year, insurance) {
     (this.base = 50000000),
       (this.carPrice = 0),
       (this.carYear = 0),
-      (this.insurance = 0);
+      (this.insurance = 0),
+      (this.car = car),
+      (this.year = year),
+      (this.kInsurance = insurance);
   }
-  calculateCar(car) {
+  calculateCar() {
     let base = this.base;
-    if (car.includes("I")) {
+    if (this.car.includes("I")) {
       this.carPrice = base + (base * 30) / 100;
     } else {
       this.carPrice = base + (base * 50) / 100;
     }
-    return this.carPrice;
+    return parseInt(this.carPrice);
   }
-  calculateYear(year) {
+  calculateYear() {
     let base = this.carPrice;
     let nowYear = new Date().getFullYear() + 1;
     // now (year) minus carBuildYear
-    let difference = nowYear - year;
+    let difference = nowYear - this.year;
     let priceOfYear = base * 0.15;
     //price of year multiply by difference and put it to this.price
     this.carYear = priceOfYear / difference;
     this.carYear += this.carPrice;
     return parseInt(this.carYear);
   }
-  calculateInsurance(option) {
+  calculateInsurance() {
     this.insurance = this.carYear;
-    if (option === "basicInsurance") {
+    if (this.kInsurance === "basicInsurance") {
       this.insurance += this.insurance * 0.3;
       return this.insurance;
     } else {
       this.insurance += this.insurance * 0.4;
-      return this.insurance;
+      return parseInt(this.insurance);
     }
+  }
+  calculate() {
+    this.calculateCar(), this.calculateYear(), this.calculateInsurance();
+    return parseInt(this.insurance);
   }
 }
 
 class HTMLShow extends Insurance {
-  constructor(car, year, insurance) {
+  constructor() {
     super();
-    (this.car = car), (this.year = year), (this.insurance = insurance);
   }
-  getCar(car) {
-    console.log(this.calculateCar(car));
-  }
+
   showYears(selectTag) {
     //show Persian Date
     let EnDate = this.getYears();
@@ -68,5 +72,67 @@ class HTMLShow extends Insurance {
     const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
 
     return year.toString().replace(/\d/g, (x) => farsiDigits[x]);
+  }
+  showTable(car, year, insurance, price) {
+    let showCar = "",
+      showInsurance = "",
+      showPrice = this.insurance;
+    switch (car) {
+      case "I1":
+        showCar = "پراید";
+        break;
+      case "I2":
+        showCar = "سمند";
+        break;
+      case "I3":
+        showCar = "پژو 206";
+        break;
+      case "I4":
+        showCar = "رانا";
+        break;
+      case "I5":
+        showCar = "تیبا";
+        break;
+      case "I6":
+        showCar = "دنا (پلاس،توربو اتوماتیک)";
+        break;
+      case "I7":
+        showCar = "شاهین";
+        break;
+      case "O1":
+        showCar = "لندکروز";
+        break;
+      case "O2":
+        showCar = "سانتافه";
+        break;
+      case "O3":
+        showCar = "بی ام و X4";
+        break;
+      case "O4":
+        showCar = "پورشه";
+        break;
+    }
+    switch (insurance) {
+      case "basicInsurance":
+        showInsurance = "بیمه ساده";
+        break;
+      case "completeInsurance":
+        showInsurance = "بیمه کامل شخص ثالث";
+        break;
+    }
+
+    let checkout = document.querySelectorAll(".checkoutForm"),
+      table = document.querySelector("#tbody");
+    checkout.forEach((element) => {
+      element.setAttribute("style", "display: flex !important");
+    });
+    table.innerHTML = `
+    <tr>
+    <td>${showCar}</td>
+    <td>${year}</td>
+    <td>${showInsurance}</td>
+    <td>${price}</td>
+    </tr>
+    `;
   }
 }
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,13 @@
 //variables
-let insurance = new Insurance(),
-  htmlShow = new HTMLShow(),
+let htmlShow = new HTMLShow(),
   selectBuiltNumber = document.querySelector("#selectCarsYear"),
   selectCar = document.querySelector("#selectCar"),
   form = document.querySelector("#form"),
   btnCalculate = document.querySelector("#calculate"),
   btnReset = document.querySelector("#formReset"),
-  formRadio = document.querySelectorAll('input[type="radio"]');
+  formRadio = document.querySelectorAll('input[type="radio"]'),
+  kInsurance = "",
+  car = "";
 
 //eventListeners
 eventListeners();
@@ -17,32 +18,55 @@ function eventListeners() {
     btnCalculate.disabled = true;
     //show years in built selection box
     htmlShow.showYears(selectBuiltNumber);
+    selectBuiltNumber.disabled = true;
+    formRadio[0].disabled = true;
+    formRadio[1].disabled = true;
+    kInsurance = formRadio[0].value;
   });
-  // change select builtYear option tag value event listeners
-  selectBuiltNumber.addEventListener("change", (option) => {
-    insurance.calculateYear(option.target.value);
+  //active button if everything fill out
+  selectCar.addEventListener("change", () => {
+    selectBuiltNumber.disabled = false;
   });
-  // change select car option tag value
-  selectCar.addEventListener("change", (option) => {
-    insurance.calculateCar(option.target.value);
-  });
-  //checked Insurance radio button and get value with eventListeners
-  formRadio.forEach((kind) => {
-    kind.addEventListener("change", (option) => {
-      insurance.calculateInsurance(option.target.value);
-    });
+  selectBuiltNumber.addEventListener("change", () => {
+    formRadio[0].disabled = false;
+    formRadio[1].disabled = false;
+    btnCalculate.disabled = false;
   });
 
-  //active button if everything fill out
-  selectCar.addEventListener("blur", () => {
-    btnCalculate.disabled = false;
+  //formRadio
+  formRadio.forEach((option) => {
+    option.addEventListener("click", (event) => {
+      if (event.target.value === "basicInsurance")
+        kInsurance = event.target.value;
+      else kInsurance = event.target.value;
+      console.log(event.target.value);
+    });
+  });
+  // show car text Content
+  selectCar.addEventListener("change", (e) => {
+    console.log(e.target.value);
   });
   //calculate
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    let insurance = new Insurance(
+      selectCar.value,
+      selectBuiltNumber.value,
+      kInsurance
+    );
+    insurance.calculate();
+    htmlShow.showTable(
+      selectCar.value,
+      selectBuiltNumber.value,
+      kInsurance,
+      insurance.calculate()
+    );
   });
   //reset form
   btnReset.addEventListener("click", () => {
     form.reset();
+    selectBuiltNumber.disabled = true;
+    formRadio[0].disabled = true;
+    formRadio[1].disabled = true;
   });
 }
